03ad919374b428b8b55191c27a39c1fe37bc7538
Arthus <531151995@qq.com>
2020-03-31 13:01:49 +0800

    log

--- themes/momentum/scripts/helper_nav.js
+++ themes/momentum/scripts/helper_nav.js
@@ -28,7 +28,9 @@ let search = function(query, prop,id) {
 
 hexo.extend.helper.register('md_list', function(page){
     let query = page.categories;
-    console.log(page.raw)
+    if(page.slug && page.slug.indexOf('event')!==-1) {
+        console.log(page.raw)
+    }
     if(query && query.length>0) {
         let post = query.data[query.length-1].posts;
         if(post.length > 0) {
